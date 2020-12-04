57dd0726689b69e474455f0445b9d27fba5e1819
aboucher <aboucher@twilio.com>
2020-12-04 14:46:44 -0800

    starter kit

---
+++ src/Participant.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import './App.scss';
+
+class Participant extends Component {
+  
+}
+
+export default Participant;
---
+++ src/Room.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import './App.scss';
+
+class Room extends Component {
+
+}
+
+export default Room;
